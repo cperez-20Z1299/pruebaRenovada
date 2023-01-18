6585c42fd9c8b9c76e3cf2f811a12b6ad66d7d46
Carlos Perez <cperez-2021299@kinal.edu.gt>
2023-01-18 14:53:36 -0600

    Primer commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function sumar(){
+    let valor1 = document.getElementById("valor1").value;
+    let valor2 = document.getElementById("valor2").value;
+
+    let suma = parseInt(numero1) + parseInt(numero2);
+    console.log(suma);
+}
